e86006d1aa6939a7c48f88692556415240e8af13
hoangit2k2 <nguyenhoang2002it@gmail.com>
2021-09-18 17:11:10 +0700

    18/09/2021
    18/09/2021

---
+++ jss/popup.js
@@ -0,0 +1,104 @@
+﻿
+//BẠN SỬA MÃNG BÊN DƯỚI, MỖI PHẦN TỬ LÀ MỘT KÍ TỰ TÍNH LUỐN KÍ TỰ TRẮNG NHÉ! BẠN CÓ THỂ THÊM NHIỀU PHẦN TỬ HƠN KHÔNG NHẤT THIẾT LÀ 28
+//XEM KĨ VIDEO CỦA MÌNH HƠN Ở PHẦN NÀY NHÉ!
+
+status="duminhphongadv"
+var a=new Array(),n=""
+a[1]='Đ';
+a[2]='ơ';
+a[3]='n';
+a[4]=' ';
+a[5]='g';
+a[6]='i';
+a[7]='ả';
+a[8]='n';
+a[9]='!';
+a[10]=' ';
+a[11]='V';
+a[12]='ì';
+a[13]=' ';
+a[14]='A';
+a[15]='N';
+a[16]='H';
+a[17]=' ';
+a[18]='H';
+a[19]='O';
+a[20]='À';
+a[21]='N';
+a[22]='G';
+a[23]=' ';
+a[24]='Đ';
+a[25]='Ẹ';
+a[26]='P';
+a[27]=' ';
+a[28]='T';
+a[29]='R';
+a[30]='A';
+a[31]='I';
+a[32]=' ';
+a[33]='H';
+a[34]='I';
+a[35]='H';
+a[36]='I';
+a[37]='♥';
+
+
+function one()
+{
+t=document.f.txt.value
+j=t.length
+if(j>0)
+{
+for(var i=1;i<=j;i++)
+{
+n=n+a[i]
+if(i==38)
+{
+document.f.txt.value=""
+n=""
+}
+}
+}
+document.f.txt.value=n
+n=""
+setTimeout("one()",1)
+}
+function s()
+{
+}
+function on()
+{
+one()
+}
+
+        $(document).ready(function() {
+            $('#k').hide();
+            $('h1').click(function() {
+                $('.active').removeClass('active');
+                $('#k').slideUp('fast');
+                if($(this).next('#k').is(':hidden') == true) {
+                $(this).addClass('active');
+                $(this).next('#k').slideDown('fast');
+                }
+            });
+        });
+					function Yeu()
+					{
+					$("#divResult").fadeOut(0);
+					//PHẦN NÀY LÀ PHẦN HIỆN KHI BẠN BẤM "GỞI ĐI" LINK Ở SAU LÀ ICON MẶT CƯỜI XÓA BỎ CŨNG ĐƯỢC CÒN TEXT THÌ BẠN SỬA LẠI THEO Ý CỦA BẠN.
+				
+					$("#divResult").fadeIn(2000,function()
+							{
+							$("#divResult2").fadeOut(0);
+							$("#divResult2").html("<p>ANH HOÀNG : ♥ ỪA THÌ ANH BIẾT ANH ĐẸP TRAI ,EM NÓI VẬY THÌ ANH CŨNG ĐÀNH YÊU EM VẬY.<img src='http://duminhphongadv.com/files/assets/4.png'/></p></br>");
+							$("#divResult2").fadeIn(2000,function()
+									{
+									$("#divResult3").fadeOut(0);
+									$("#divResult3").html("<p>VẬY HÃY ĐỂ TRÁI TIM ANH THUỘC VỀ EM  NHÉ!  <img src='http://duminhphongadv.com/files/assets/5.png'/></p></br>");
+									$("#divResult3").fadeIn(2000);
+									}
+								);
+							}
+						);
+					}
+				
